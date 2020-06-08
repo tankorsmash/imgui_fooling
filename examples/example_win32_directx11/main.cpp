be4add8f358ed7a545afbe81519d6a876a9a595c
tankorsmash <tankorsmash@gmail.com>
2020-06-08 15:49:35 -0400

    convert delaunay helpers into functions instead of lambdas

--- examples/example_win32_directx11/main.cpp
+++ examples/example_win32_directx11/main.cpp
@@ -729,6 +729,64 @@ int pnpoly(int num_verts, double *vert_xs, double *vert_ys, double point_x, doub
     return c;
 }
 
+edge_t nextHalfEdge(edge_t edge)
+{
+    return (edge % 3 == 2) ? edge - 2 : edge + 1;
+};
+
+edge_t triangleOfEdge(edge_t e) {
+    return e / 3;
+};
+
+//returns 3 edge IDs
+std::vector<edge_t> edgesOfTriangle(edge_t t) {
+    return std::vector<edge_t>{3 * t, 3 * t + 1, 3 * t + 2};
+};
+
+//finds 3 points for a given triangle id
+std::vector<edge_t> pointsOfTriangle(const delaunator::Delaunator& delaunator, edge_t tri_id){
+    auto edges = edgesOfTriangle(tri_id);
+
+    std::vector<size_t> points;
+    points.reserve(3);
+    auto find_tri_for_edge = [&delaunator](edge_t e) -> size_t{ return delaunator.triangles[e]; };
+    std::transform(edges.begin(), edges.end(), std::back_inserter(points), find_tri_for_edge);
+
+    return points;
+};
+
+std::vector<edge_t> edgesAroundPoint(const delaunator::Delaunator& delaunator, edge_t start) {
+    std::vector<edge_t> result;
+
+    edge_t incoming = start;
+    do {
+        result.push_back(incoming);
+        const edge_t outgoing = nextHalfEdge(incoming);
+        incoming = delaunator.halfedges[outgoing];
+        //} while (incoming != (edge_t)-1 && incoming != start);
+    } while (incoming != delaunator::INVALID_INDEX && incoming != start);
+
+    return result;
+};
+
+//circumcenter of triangle
+std::pair<double, double> triangleCenter(delaunator::Delaunator& delaunator, edge_t tri_id)
+{
+    auto tri_points = pointsOfTriangle(delaunator, tri_id);
+    std::vector<std::pair<double, double>> vertices{};
+    std::transform(tri_points.begin(), tri_points.end(), std::back_inserter(vertices), [&delaunator](edge_t tri_point) {
+        return double_pair_t{delaunator.coords.at(tri_point * 2), delaunator.coords.at(tri_point * 2 + 1)};
+    });
+
+    //auto result = circumcenter(vertices[0], vertices[1], vertices[2]);
+    auto result = delaunator::circumcenter(
+        vertices[0].first, vertices[0].second,
+        vertices[1].first, vertices[1].second,
+        vertices[2].first, vertices[2].second
+        );
+    return std::pair<double, double>(result.first, result.second);
+};
+
 int main(int, char**)
 {
     // Create application window
@@ -792,60 +850,6 @@ int main(int, char**)
     regenerate_canvas();
 
 
-    auto nextHalfEdge = [](edge_t edge) { return (edge % 3 == 2) ? edge - 2 : edge + 1;  };
-
-    auto triangleOfEdge = [](edge_t e) {
-        return e / 3;
-    };
-
-    //returns 3 edge IDs
-    auto edgesOfTriangle = [](edge_t t) -> std::vector<edge_t> {
-        return std::vector<edge_t>{3* t, 3* t + 1, 3 * t + 2};
-    };
-
-    //finds 3 points for a given triangle id
-    auto pointsOfTriangle = [edgesOfTriangle](const delaunator::Delaunator& delaunator, edge_t tri_id) ->std::vector<size_t> {
-        auto edges = edgesOfTriangle(tri_id);
-
-        std::vector<size_t> points;
-        points.reserve(3);
-        auto find_tri_for_edge = [&delaunator](edge_t e) -> size_t{ return delaunator.triangles[e]; };
-        std::transform(edges.begin(), edges.end(), std::back_inserter(points), find_tri_for_edge);
-
-        return points;
-    };
-
-    auto edgesAroundPoint = [nextHalfEdge](const delaunator::Delaunator& delaunator, edge_t start) {
-        std::vector<edge_t> result;
-
-        edge_t incoming = start;
-        do {
-            result.push_back(incoming);
-            const edge_t outgoing = nextHalfEdge(incoming);
-            incoming = delaunator.halfedges[outgoing];
-        //} while (incoming != (edge_t)-1 && incoming != start);
-        } while (incoming != delaunator::INVALID_INDEX && incoming != start);
-
-        return result;
-    };
-
-    //circumcenter of triangle
-    auto triangleCenter = [pointsOfTriangle](delaunator::Delaunator& delaunator, edge_t tri_id) -> std::pair<double, double>
-    {
-        auto tri_points = pointsOfTriangle(delaunator, tri_id);
-        std::vector<std::pair<double, double>> vertices{};
-        std::transform(tri_points.begin(), tri_points.end(), std::back_inserter(vertices), [&delaunator](edge_t tri_point) {
-            return double_pair_t{delaunator.coords.at(tri_point*2), delaunator.coords.at(tri_point*2 + 1)};
-        });
-
-        //auto result = circumcenter(vertices[0], vertices[1], vertices[2]);
-        auto result = delaunator::circumcenter(
-            vertices[0].first, vertices[0].second,
-            vertices[1].first, vertices[1].second,
-            vertices[2].first, vertices[2].second
-        );
-        return std::pair<double, double>(result.first, result.second);
-    };
 
     auto forEachVoronoiEdge = [&](delaunator::Delaunator& delaunator, std::function<void(edge_t, double_pair_t, double_pair_t)> callback) {
         //forEachTriangleEdge
@@ -876,7 +880,7 @@ int main(int, char**)
         }
     };
 
-    auto forEachVoronoiCell = [nextHalfEdge, edgesAroundPoint, triangleOfEdge, triangleCenter](
+    auto forEachVoronoiCell = [](
         delaunator::Delaunator& delaunator,
         std::function<void(edge_t, std::vector<coord_t>&)> callback) {
 
@@ -899,7 +903,7 @@ int main(int, char**)
                 std::vector<coord_t> vertices{};
                 std::transform(
                     BEND(triangles), std::back_inserter(vertices),
-                    [&triangleCenter, &delaunator](edge_t tri_id) {
+                    [&delaunator](edge_t tri_id) {
                         auto result = triangleCenter(delaunator, tri_id);
                         return result;
                     }
@@ -930,7 +934,7 @@ int main(int, char**)
             
     };
 
-    auto forEachVoronoiTriangle = [nextHalfEdge, edgesAroundPoint, triangleOfEdge, triangleCenter, pointsOfTriangle](
+    auto forEachVoronoiTriangle = [](
         delaunator::Delaunator& delaunator,
         std::function<void(edge_t, std::vector<double>)> callback) {
 
